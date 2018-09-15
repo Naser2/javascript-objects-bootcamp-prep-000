01952ad59055fb7c7241f00e9839c5fac464663a
Nasser Sanou <nassersanou23@gmail.com>
2018-09-15 07:13:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,8 @@
  song: 'I wanna  love you '
  }
    
+   // objects updatePlaylist(playlist, artistName, songTitle) adds the `artistName: songTitle` key-value pair to `playlist`:
+  playlist[arti
 const updatePlaylist = function(playlist, artistName, songTitle)  {
 // objects updatePlaylist(playlist, artistName, songTitle) adds the `artistName: songTitle` key-value pair to `playlist`:
   playlist[artistName] = songTitle
