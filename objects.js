184c160e2b4af6a990788617b5ced122e63915c7
Nasser Sanou <nassersanou23@gmail.com>
2018-09-15 07:14:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@
    
 // 2 objects updatePlaylist(playlist, artistName, songTitle) adds the `artistName: songTitle` key-value pair to `playlist`:
 
-const updatePlaylist = function(playlist, artistName, songTitle)  {
+const updatePlaylist = function updatePlaylist(playlist, artistName, songTitle)  {
   playlist[artistName] = songTitle
 
   return playlist
